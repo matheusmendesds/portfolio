48cd4941021cf689362995ee089cbec359810e2a
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2024-01-19 19:51:30 -0300

    arrumando a page inicial

---
+++ src/components/layout/Socials.tsx
@@ -0,0 +1,9 @@
+import "@/components/layout/Socials.module.css"
+
+export default function Socials(){
+    return (
+        <div className="section_socials">
+            <p>teste</p>
+        </div>
+    )
+}
--- src/pages/Inicio/Inicio.tsx
+++ src/pages/Inicio/Inicio.tsx
@@ -1,19 +1,23 @@
 import Image from "next/image"
+import Socials from "@/components/layout/Socials"
+
 export default function Inicio(){
 
 
     return (
-        <section id="inicio"  className="h-screen bg-roxoPrincipal flex justify-center items-center">
-            <div id="teste" className="card_info font-TextLibreCaslon text-white ">
-                <p className="card_title">Matheus Mendes</p>
-                <p className="card_title">dos Santos</p>
-                <Image src="Image/laptop.png" width={30} height={30}/>
+        <div id="inicio"  className="h-screen bg-roxoPrincipal flex justify-center" >
+            <section className="card_info font-Roboto text-white ">
+                <div>
+                    <p className="card_title">Matheus Mendes</p>
+                    <p className="card_title">dos Santos</p>
+                </div>
                 <div className="flex gap-1">
                     <p>Desenvolvedor Front-End</p><span>|</span>
                     <p>Estudante de Análise e Desenvolvimento de Sistemas</p>
                 </div>
-                {/*  bg-[url('/bgpurple.jpg')]  w-[100%] bg-cover bg-center */}
-            </div>
-        </section>
+            </section>
+            <Socials/>
+            
+        </div>
     )
 }
