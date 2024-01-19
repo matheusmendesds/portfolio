9fe01e2ec5f2c43d0accd7f6428368d24287a5b6
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2024-01-19 16:39:13 -0300

    att pagina de inicio

--- src/pages/Inicio/Inicio.tsx
+++ src/pages/Inicio/Inicio.tsx
@@ -1,13 +1,17 @@
-
+import Image from "next/image"
 export default function Inicio(){
 
 
     return (
         <section id="inicio"  className="h-screen bg-roxoPrincipal flex justify-center items-center">
             <div id="teste" className="card_info font-TextLibreCaslon text-white ">
-                <h2 className="text-4xl">Matheus Mendes dos Santos</h2>
-                <p>Desenvolvedor Front-End</p>
-                <p>Estudante de Análise e Desenvolvimento de Sistemas</p>
+                <p className="card_title">Matheus Mendes</p>
+                <p className="card_title">dos Santos</p>
+                <Image src="Image/laptop.png" width={30} height={30}/>
+                <div className="flex gap-1">
+                    <p>Desenvolvedor Front-End</p><span>|</span>
+                    <p>Estudante de Análise e Desenvolvimento de Sistemas</p>
+                </div>
                 {/*  bg-[url('/bgpurple.jpg')]  w-[100%] bg-cover bg-center */}
             </div>
         </section>
