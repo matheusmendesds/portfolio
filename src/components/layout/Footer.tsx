bba4899a5a8f42a24eed83d80141b97c2b1357d0
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2024-01-29 15:37:30 -0300

    Delete Footer.tsx

--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function() {
-    return (
-        <footer className="font-Header bg-purple-900">
-          <p>Projeto criado por Matheus Mendes dos Santos</p>
-        </footer>  
-    )
-
-    
-}
